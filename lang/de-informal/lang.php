9b5420def5102a249bb164585f228ad3eceb8f59
Daniel Weisshaar <wda@hs-furtwangen.de>
2022-03-17 01:25:26 +0100

    Added informal German language

---
+++ lang/de-informal/lang.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Daniel Weisshaar <daniwei-dev@gmx.de>
+ */
+$lang['e_pagenotexists']       = 'Diese Seite [%s] existiert nicht, kann nicht geändert werden';
+$lang['e_denied']              = 'Du bist nicht berechtigt, diese Seite zu ändern. Hast du vielleicht vergessen, dich einzuloggen?';
+$lang['e_template']            = 'Konnte die zu ändernde Seite "%s" nicht einlesen. Existiert sie eventuell nicht?';
+$lang['e_failedtoparse']       = 'Die Seite konnte nicht geändert werden';
+$lang['summary']               = 'Geändert durch das Formular um %s';
+$lang['pleasewait']            = 'Bitte warte, solange die Seite deine Ergebnisse verarbeitet.... %s oder klicke auf %s, um sie zu sehen';
+$lang['success']               = 'Erfolgreich verarbeitet';
---
+++ lang/de-informal/settings.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * english language file for pagemod plugin
+ * 
+ * @author Daniel Weisshaar
+ */
+$lang['ignore_acl'] = 'Seitenänderungen für alle Nutzer auf allen Seiten erlauben, ohne die Zugriffsrechte zu beachten?';
+?>
